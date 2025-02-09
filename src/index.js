2d27a90c6cd1008a6f302b9076410c0d156086bc
bskandraaj <23r21a1206@mlrit.ac.in>
2025-02-09 21:43:37 +0530

    Navbar commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
->>>>>>> 7d1b4fb34bc363af47dc7e03c245fdc7ddf056de
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -10,7 +10,7 @@ const Signup=()=>{
     
     return(
         <>
-        Signup page
+        
         </>
     )
 }
